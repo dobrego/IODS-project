bf7125449e596846efb016684e1be56f9e3c74fa
Aleksandra Dobrego <sashadobrego@Aleksandras-MacBook-Air.local>
2019-11-11 20:04:22 +0200

    R Studio Exercise 2: Analysis

--- data/create_learning2014.R
+++ data/create_learning2014.R
@@ -41,3 +41,69 @@ read.table("learning2014.csv")
 #making sure the data is correct
 str(read.table("learning2014.csv"))
 head(read.table("learning2014.csv"))
+
+#November 11, 2019
+#It's an R Studio Exercise 2: Part 2 (Analysis)
+
+#1
+#reading a file
+my_learning <- read.table("learning2014.csv")
+#exploring structure and dimensions
+str(my_learning) 
+#3 numeric variables (deep = deep learning, stra = strategic learning, surf = surface learning), 3 integer variables (points, age, attitude) and 1 factor variable (gender)
+dim(my_learning) #the dataset has 166 observations and 7 variables
+
+#2
+#graphical overview of the data: Attitude and Points coloured by gender
+ggplot(my_learning, aes(x = Attitude, y = Points, col = gender)) +
+  geom_point() 
+#summaries of variables
+summary(my_learning) # we have twice more males than females
+#distribution of other variables
+hist(my_learning$Age) #not normal
+hist(my_learning$Attitude) #normal
+hist(my_learning$deep) #not normal
+hist(my_learning$stra) #?? 
+hist(my_learning$surf) #normal
+hist(my_learning$Points) #normal
+#relationship between variables
+ggpairs(my_learning, mapping = aes(col = gender, alpha = 0.3), lower = list(combo = wrap("facethist", bins = 20)))
+#conclusion of variables: correlation is actually very low for all variables, but the highest is for attitude&points
+
+#3
+#multiple regression model with attutude, strategic questions and deep learning questions; exam points is a dependent variable
+model <- lm(Points ~ Attitude + stra + deep, data = my_learning)
+#summary of the model
+summary(model)
+#interpretation of model results and stats: 
+#p value is very low, which means hat, at least, one of the predictor variables is significantly related to the outcome variable
+#only attitude can explain points well, that is, changes is attitude will affect points
+#as others are not significating, we can remove them from the model
+#removing stra from the model
+modelnostra <- lm(Points ~ Attitude + deep, data = my_learning)
+summary(modelnostra)
+#also removing deep from the model
+modelnostranodeep <- lm(Points ~ Attitude, data = my_learning)
+summary(modelnostranodeep) #it is the best model (it minimizes the prediction errors = residuals)
+
+#4
+#i am going back to the original model and explaining it
+model <- lm(Points ~ Attitude + stra + deep, data = my_learning)
+summary(model)
+#relationship between variables and the target (null hypothesis: actual value of Attitude is 0)
+#only changes in attitude affect the exam points
+#interpreting the multiple R-squared: 
+#correlation coefficient between observed values and predicted values
+#here R = 0.2097, which means that only 20% of of the variance in the points can be explained by chosen variables
+
+#5
+model <- lm(Points ~ Attitude + stra + deep, data = my_learning)
+#plot of Residuals vs Fitted Values: used to explore whether errors depend on explanatory variables
+plot(model, which = c(1))
+#our resfit plot looks reasonable
+#normal QQplot: used to explore whether the errors are normally distributed
+plot(model, which = c(2))
+#our QQplot looks reasonable
+#plot of residuals vs leverage: identify which observations have unusually high impact on the model
+plot(model, which = c(5))
+#our plot looks bad: there is an outlier and leverage is high
